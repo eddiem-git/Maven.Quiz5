fb9db1951dd658672f406bff323ce4bf8c6f87ea
eddie <eddiem9657@gmail.com>
2020-01-03 14:58:46 -0500

    86/234

--- src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
@@ -1,22 +1,29 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class ZipCodeWilmington {
+    List<Student> students;
+    double totalLectureTime;
     public void enroll(Student student) {
+        students.add(student);
+
     }
 
     public Boolean isEnrolled(Student student) {
-        return null;
+        return students.contains(student);
     }
 
     public void lecture(double numberOfHours) {
+        this.totalLectureTime += numberOfHours;
     }
 
     public Map<Student, Double> getStudyMap() {
+
         return null;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
@@ -6,25 +6,21 @@ import rocks.zipcode.io.quiz4.fundamentals.StringEvaluator;
  * @author leon on 19/12/2018.
  */
 public class StringEvaluatorObject {
-    String str1;
-    String str2;
+    String currentString;
+
     public StringEvaluatorObject(String str) {
-        this.str1 = str;
-    }
-    public StringEvaluatorObject(String str1, String str2){
-        this.str1 = str1;
-        this.str2 = str2;
+        this.currentString = str;
     }
 
     public String[] getAllSubstrings() {
-        return StringEvaluator.getAllSubstrings(str1);
+        return StringEvaluator.getAllSubstrings(currentString);
     }
 
     public String[] getCommonSubstrings(String secondInput) {
-        return StringEvaluator.getCommonSubstrings(secondInput,str1);
+        return StringEvaluator.getCommonSubstrings(currentString, secondInput);
     }
 
     public String getLargestCommonSubstring(String secondInput) {
-        return StringEvaluator.getLargestCommonSubstring(secondInput, str1);
+        return StringEvaluator.getLargestCommonSubstring(currentString, secondInput);
     }
 }
