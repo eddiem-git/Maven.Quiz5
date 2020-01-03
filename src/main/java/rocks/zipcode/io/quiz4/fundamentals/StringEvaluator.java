90a5175f76309ab96445d40ccee5aca717856b69
eddie <eddiem9657@gmail.com>
2020-01-03 14:47:15 -0500

    Fundamentals

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,8 +1,6 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author leon on 11/12/2018.
@@ -21,14 +19,27 @@ public class StringEvaluator {
     }
 
     public static String[] getCommonSubstrings(String s1, String s2) {
-        for( int i = Math.min(s1.length(), s2.length()); ; i--) {
-        if(s2.endsWith(s1.substring(0, i))) {
-            String[] strings = {s1.substring(0, i)};
+        Set<String> a = new HashSet<>(Arrays.asList(getAllSubstrings(s1)));
+        Set<String> b = new HashSet<>(Arrays.asList(getAllSubstrings(s2)));
+
+        ArrayList<String> result = new ArrayList<>();
+        for (String each: a) {
+            if(b.contains(each))
+                result.add(each);
         }
+        return result.toArray(new String[0]);
+
+
     }
-}
 
     public static String getLargestCommonSubstring(String string1, String string2) {
-        return null;
+        String[] commonSubStrings = getCommonSubstrings(string1, string2);
+        String largest = commonSubStrings[0];
+        for(int i = 1; i < commonSubStrings.length; i++){
+            if(commonSubStrings[i].length() > largest.length()){
+                largest = commonSubStrings[i];
+            }
+        }
+        return largest;
     }
 }
